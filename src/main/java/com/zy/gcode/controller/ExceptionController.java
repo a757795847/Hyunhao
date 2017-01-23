35ebf68ad43da4087ac4bc962af35b9d9aa445af
admin5 <admin>
2017-01-23 17:23:59 +0800

    添加了红包状态查询功能，并把红包发送信息存入db

--- src/main/java/com/zy/gcode/controller/ExceptionController.java
+++ src/main/java/com/zy/gcode/controller/ExceptionController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * Created by admin5 on 17/1/23.
  */
 @Controller
-public class ErrorController{
+public class ExceptionController {
     @RequestMapping("error")
     public @ResponseBody String error(String message){
         return message;
