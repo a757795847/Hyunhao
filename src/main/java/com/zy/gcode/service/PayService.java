8ee64a920d015b393a4b2edec0222c9410849b6f
admin5 <admin>
2017-01-24 15:44:33 +0800

    添加了红包状态查询功能，并把红包发送信息存入db

--- src/main/java/com/zy/gcode/service/PayService.java
+++ src/main/java/com/zy/gcode/service/PayService.java
@@ -42,8 +42,8 @@ public class PayService implements IPayService {
         payInfo.setWxappid("wx653d39223641bea7");
         payInfo.setSend_name("追游科技");
         payInfo.setRe_openid(openid);
-        payInfo.setTotal_num(100);
-        payInfo.setTotal_amount(1);
+        payInfo.setTotal_num(1);
+        payInfo.setTotal_amount(10);
         payInfo.setWishing(strs[(int)(Math.random()*8)]);
         payInfo.setClient_ip("115.29.188.190");
         payInfo.setAct_name("好评返现");
