b207aae059c59f216452d3c432e4c9fd5be7a1da
桂敏瑞 <3438889665@qq.com>
2017-03-06 17:26:14 +0800

    添加 orderController -> redInfo（获取红包详情）

--- src/main/java/com/zy/gcode/utils/Constants.java
+++ src/main/java/com/zy/gcode/utils/Constants.java
@@ -48,13 +48,13 @@ public abstract class Constants {
      * 上传的csv存放路径
      */
 
-    public final static String RED_CSV_PATH = "/Users/admin3/Downloads";
+    public final static String RED_CSV_PATH = "/opt/zy/Downloads";
 
     /**
      * 支付二维码存放路径
      */
 
-    public final static String PAY_QR_PATH = "/Users/admin3/Downloads";
+    public final static String PAY_QR_PATH = "/opt/zy/Downloads";
 
     /**
      * 这个应用的id
