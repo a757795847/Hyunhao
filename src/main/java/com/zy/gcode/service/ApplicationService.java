4d55d7d7486821f61502c35b6bd3345fe30ef575
桂敏瑞 <3438889665@qq.com>
2017-04-05 19:01:26 +0800

    添加错误订单下载功能

--- src/main/java/com/zy/gcode/service/ApplicationService.java
+++ src/main/java/com/zy/gcode/service/ApplicationService.java
@@ -207,7 +207,7 @@ public class ApplicationService implements IApplicationService {
         }
 
     }
-    String httpPrefix = "http://open.izhuiyou.com/app/";
+    String httpPrefix = "http://open.izhuiyou.com/wechat/view/home";
 
     @Override
     @Transactional
