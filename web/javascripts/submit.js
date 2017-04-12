7671dd8fbb2a37940f7dbf928dd3a3e523238b9c
hyh <dfhjdfh@163.com>
2017-04-12 14:24:10 +0800

    hyh

--- web/javascripts/submit.js
+++ web/javascripts/submit.js
@@ -2,7 +2,7 @@ var config = $("#config-json").data("json");
 console.log(config);
 // console.info("config: " + config);
 wx.config({
-    debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+    debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
     appId: config.appid, // 必填，公众号的唯一标识
     timestamp: config.timestamp, // 必填，生成签名的时间戳
     nonceStr: config.nonceStr, // 必填，生成签名的随机串
@@ -29,8 +29,9 @@ $("#odd").on("click",function(){
                     success: function (res) {
                         var serverId = res.serverId; // 返回图片的服务器端ID
                         console.log(serverId);
-
+                            
                             ids.push(serverId);
+                        console.log(ids);
                         var image='<li class="lict"><img src="'+id+'" alt=""></li>';
                         if(ids.length == 3){
                             $("#odd").hide();
